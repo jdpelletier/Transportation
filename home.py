041e1a66215354bb6c0b4b8eadd4ee7f1991b2d6
jdpelletier <john.pelletier@ymail.com>
2021-04-05 01:19:13 -1000

    first mockup

--- home.py
+++ home.py
@@ -1,33 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
-def home():
-    k2_assigned = "njordan"
-    k3_assigned = "jpelletier"
-    k4_assigned = "N/A"
-    k2_problem = False
-    k3_problem = False
-    k4_problem = True
-    k2_location = "Waimea"
-    k3_location = "Hilo"
-    k4_location = "HP"
-    K2 = {
-        "name": "K4",
-        "location": k2_location,
-        "assigned": k2_assigned,
-        "issue": k2_problem
-    }
-    K3 = {
-        "name": "K4",
-        "location": k3_location,
-        "assigned": k3_assigned,
-        "issue": k3_problem
-    }
-    K4 = {
-        "name": "K4",
-        "location": k4_location,
-        "assigned": k4_assigned,
-        "issue": k4_problem
-    }
-    cars = [K2, K3, K4]
-
+def home(cars):
     return render_template("home.html", cars=cars)
--- transportation_web_server.py
+++ transportation_web_server.py
@@ -10,10 +10,52 @@ bootstrap = Bootstrap(app)
 
 from home import home
 
+k2_assigned = "njordan"
+k3_assigned = "jpelletier"
+k4_assigned = "N/A"
+k2_problem = False
+k3_problem = False
+k4_problem = True
+k2_location = "Waimea"
+k3_location = "Hilo"
+k4_location = "HP"
+k2_report = "n/a"
+k3_report = "n/a"
+k4_report = "This car is missing it's rearview mirror. -jpelletier 04/05/21"
+K2 = {
+    "name": "K4",
+    "location": k2_location,
+    "assigned": k2_assigned,
+    "issue": k2_problem,
+    "issue_report": k2_report
+}
+K3 = {
+    "name": "K4",
+    "location": k3_location,
+    "assigned": k3_assigned,
+    "issue": k3_problem,
+    "issue_report": k3_report
+}
+K4 = {
+    "name": "K4",
+    "location": k4_location,
+    "assigned": k4_assigned,
+    "issue": k4_problem,
+    "issue_report": k4_report
+}
+cars = [K2, K3, K4]
+
 @app.route('/',methods=['POST', 'GET'])
 def home_():
-    return home()
+    return home(cars)
 
+@app.route('/report_issue',methods=['POST', 'GET'])
+def report_issue_():
+    return render_template("report_issue.html")
+
+@app.route('/view_issues',methods=['POST', 'GET'])
+def viewissues_():
+    return render_template("viewissues.html", cars=cars)
 
 if __name__ == '__main__':
     from waitress import serve
