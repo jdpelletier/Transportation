837119f88739f4f40857bd46cfdc094887323e92
jdpelletier <john.pelletier@ymail.com>
2021-04-26 14:50:38 -1000

    Starting createrideboard

---
+++ creatRideBoard.py
@@ -0,0 +1,12 @@
+import pymysql.cursors
+import datetime.datetime
+
+today = datetime.now().strftime("%Y-%m-%d")
+
+conn = pymysql.connect(user='sched', password='sched', host='mysqlserver', database='schedules', autocommit=True)
+cur.execute(f'select * from dailySched where Date={today}')
+rows = cur.fetchall()
+
+for row in rows:
+    if row[6] == 'SU':
+        print(row[2])
