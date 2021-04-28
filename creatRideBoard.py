baa503478fdf50827966d2dfe43594543d7d9efb
jdpelletier <john.pelletier@ymail.com>
2021-04-27 14:26:59 -1000

    Update creatRideBoard.py

--- creatRideBoard.py
+++ creatRideBoard.py
@@ -5,7 +5,7 @@ today = datetime.now().strftime("%Y-%m-%d")
 
 conn = pymysql.connect(user='sched', password='sched', host='mysqlserver', database='schedules', autocommit=True)
 cur = conn.cursor()
-cur.execute(f'select * from dailySched where Date="{today}" order by Basecamp, startTime, Name')
+cur.execute(f'select * from dailySched where Date="{today}" and DelFlag = 0 order by Basecamp, startTime, Name')
 rows = cur.fetchall()
 
 employees_list = []
