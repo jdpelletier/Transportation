09da4d106fa9b8ccb5b79d820c738eeadf047e91
jdpelletier <john.pelletier@ymail.com>
2021-04-27 15:01:35 -1000

    Update creatRideBoard.py

--- creatRideBoard.py
+++ creatRideBoard.py
@@ -1,5 +1,8 @@
 import pymysql.cursors
 from datetime import datetime, timedelta
+import smtplib
+from email.mime.text import MIMEText
+
 todaydate = datetime.now() + timedelta(days=1)
 today = todaydate.strftime("%Y-%m-%d")
 
@@ -26,13 +29,29 @@ for employee in employees_list:
 
 location = ''
 time = ''
+content = f"A rideboard has been created for {today}."
 print(f"A rideboard has been created for {today}.")
 for employee in employees_list:
     if (employee['pickup'] != location) or (employee['time'] != time):
         location = employee['pickup']
         time = employee['time']
+        line = f"{location} to SU {time}\n"
+        content + = line
         print(f"{location} to SU {time}")
     if 'HP' in employee['note']:
+        line = f"     {employee['name']} (HPP)\n"
+        content + = line
         print(f"     {employee['name']} (HPP)")
     else:
+        line = f"     {employee['name']} (HPP)\n"
+        content + = line
         print(f"     {employee['name']}")
+
+
+msg = MIMEText(content)
+msg['Subject'] =f"Rideboard for {today}"
+msg['To'] = 'jpelletier@keck.hawaii.edu'
+msg['From'] = 'jpelletier@keck.hawaii.edu'
+s = smtplib.SMTP('localhost')
+s.send_message(msg)
+s.quit()
